458205702b2c4afa9828df2f710992d2bd1894bc
mikfairo <mikfairo123@gmail.com>
2025-11-19 09:08:28 -0500

    Added some imports and formatted

--- backend/data/all-ingredients-data.js
+++ backend/data/all-ingredients-data.js
@@ -1,10 +1,11 @@
 import dairyIngredients from "./dairy-ingredients-data";
 import meatIngredients from "./meat-ingredients-data";
+import vegetableIngredients from "./vegetable-ingredients-data";
 
 
 const allIngredients = {
     dairy: dairyIngredients,
     meat: meatIngredients,
-
+    vegetable: vegetableIngredients,
 }
 export default allIngredients;
--- backend/data/meat-ingredients-data.js
+++ backend/data/meat-ingredients-data.js
@@ -30,25 +30,29 @@ const meatIngredients = {
       "https://preferredmeatsinc.com/wp-content/uploads/2017/06/short-ribs.jpg",
     measurement: "",
   },
-  groundBeef: { 
-    name: "Ground Beef", 
-    thumbnail: "https://img.freepik.com/premium-photo/raw-mince-beef-ground-meat-isolated-white-background_89816-45192.jpg",
-    measurement: "", 
+  groundBeef: {
+    name: "Ground Beef",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/raw-mince-beef-ground-meat-isolated-white-background_89816-45192.jpg",
+    measurement: "",
   },
-  porkBelly: { 
-    name: "Pork Belly", 
-    thumbnail: "https://www.shutterstock.com/image-photo/streaky-pork-isolated-on-white-260nw-2273600345.jpg",
-    measurement: "", 
+  porkBelly: {
+    name: "Pork Belly",
+    thumbnail:
+      "https://www.shutterstock.com/image-photo/streaky-pork-isolated-on-white-260nw-2273600345.jpg",
+    measurement: "",
   },
-  groundPork: { 
-    name: "Ground Pork", 
-    thumbnail: "https://img.freepik.com/premium-photo/raw-meat-fresh-minced-pork-plate-isolated-white-background_106630-2393.jpg",
-    measurement: "", 
+  groundPork: {
+    name: "Ground Pork",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/raw-meat-fresh-minced-pork-plate-isolated-white-background_106630-2393.jpg",
+    measurement: "",
   },
-  porkChop: { 
-    name: "Pork Chop", 
-    thumbnail: "https://t4.ftcdn.net/jpg/00/14/52/57/360_F_14525796_xgiHs1TmwKeB6SDP2wBBOmkknYR8Py5D.jpg",
-    measurement: "", 
+  porkChop: {
+    name: "Pork Chop",
+    thumbnail:
+      "https://t4.ftcdn.net/jpg/00/14/52/57/360_F_14525796_xgiHs1TmwKeB6SDP2wBBOmkknYR8Py5D.jpg",
+    measurement: "",
   },
 };
 
