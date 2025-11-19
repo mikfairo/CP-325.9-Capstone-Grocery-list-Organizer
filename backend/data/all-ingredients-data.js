3feb95a6fbb7cf405e5d53a3c652098c30ca3268
mikfairo <mikfairo123@gmail.com>
2025-11-18 20:30:43 -0500

    Organized and adjusted files to stay modular

---
+++ backend/data/all-ingredients-data.js
@@ -0,0 +1,10 @@
+import dairyIngredients from "./dairy-ingredients-data";
+import meatIngredients from "./meat-ingredients-data";
+
+
+const allIngredients = {
+    dairy: dairyIngredients,
+    meat: meatIngredients,
+
+}
+export default allIngredients;
---
+++ backend/data/dairy-ingredients-data.js
@@ -0,0 +1,41 @@
+const dairyIngredients = {
+  type: "Dairy",
+  butter: {
+    name: "Butter",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/02/40/14/70/360_F_240147049_1DKgPqORVBXfHdqh7YZvCgDbQl7LVptM.jpg",
+    measurement: "",
+  },
+  unsaltedButter: {
+    name: "Unsalted Butter",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/02/40/14/70/360_F_240147049_1DKgPqORVBXfHdqh7YZvCgDbQl7LVptM.jpg",
+    measurement: "",
+  },
+  milk: {
+    name: "Milk",
+    thumbnail:
+      "https://t4.ftcdn.net/jpg/04/12/33/65/360_F_412336563_qJYTviZLgFoi0dld0nUdwVjci3KMA16q.jpg",
+    measurement: "",
+  },
+  sourCream: {
+    name: "Sour Cream",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/sour-cream-bowl_88281-2014.jpg",
+    measurement: "",
+  },
+  heavyCream: {
+    name: "Heavy Cream",
+    thumbnail:
+      "https://thumbs.dreamstime.com/b/glass-fresh-organic-buttermilk-isoloated-white-background-glass-fresh-buttermilk-126547324.jpg",
+    measurement: "",
+  },
+  cheddarCheese: {
+    name: "Cheddar Cheese",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/15/22/91/68/360_F_1522916820_ig0lQejVITe2lFuuwuAIkENkj68bkxAa.jpg",
+    measurement: "",
+  },
+};
+
+export default dairyIngredients;
