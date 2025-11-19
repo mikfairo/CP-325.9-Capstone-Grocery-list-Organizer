c99e0121ca052205c14ea6ad6faed916ac48d21a
mikfairo <mikfairo123@gmail.com>
2025-11-18 20:22:18 -0500

    Created backend files, connected to MongoDb and added dairy, meat ingredients to ingredient data set

---
+++ backend/data/ingredients-data.js
@@ -0,0 +1,95 @@
+const ingredients = [dairyIngredients, meatIngredients];
+
+const dairyIngredients = {
+  type: "Dairy",
+  butter: {
+    name: "Butter",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/02/40/14/70/360_F_240147049_1DKgPqORVBXfHdqh7YZvCgDbQl7LVptM.jpg",
+    measurement: "",
+  },
+  unsaltedButter: {
+    name: "Unsalted Butter",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/02/40/14/70/360_F_240147049_1DKgPqORVBXfHdqh7YZvCgDbQl7LVptM.jpg",
+    measurement: "",
+  },
+  milk: {
+    name: "Milk",
+    thumbnail:
+      "https://t4.ftcdn.net/jpg/04/12/33/65/360_F_412336563_qJYTviZLgFoi0dld0nUdwVjci3KMA16q.jpg",
+    measurement: "",
+  },
+  sourCream: {
+    name: "Sour Cream",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/sour-cream-bowl_88281-2014.jpg",
+    measurement: "",
+  },
+  heavyCream: {
+    name: "Heavy Cream",
+    thumbnail:
+      "https://thumbs.dreamstime.com/b/glass-fresh-organic-buttermilk-isoloated-white-background-glass-fresh-buttermilk-126547324.jpg",
+    measurement: "",
+  },
+  cheddarCheese: {
+    name: "Cheddar Cheese",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/15/22/91/68/360_F_1522916820_ig0lQejVITe2lFuuwuAIkENkj68bkxAa.jpg",
+    measurement: "",
+  },
+};
+
+const meatIngredients = {
+  type: "Meat",
+  chickenBreast: {
+    name: "Chicken breast",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/fresh-raw-chicken-breast-fillet-isolated-white-background-clipping-path_154092-14310.jpg",
+    mesaurement: "",
+  },
+  chickenWings: {
+    name: "Chicken Wings",
+    thumbnail:
+      "https://img.freepik.com/premium-photo/raw-chicken-wings-with-skin-white-background_280340-836.jpg",
+    measurement: "",
+  },
+  chickenThighs: {
+    name: "Chicken Thighs",
+    thumbnail:
+      "https://www.gfsstore.com/wp-content/uploads/2023/03/371108-RAW-Chicken-thigh-meat-isolated-on-white-background.-1401936268.jpeg",
+    measurement: "",
+  },
+  chuckRoast: {
+    name: "Chuck Roast beef ",
+    thumbnail:
+      "https://t4.ftcdn.net/jpg/15/33/03/73/360_F_1533037350_KrgXNFxiv6aYzLONAcfCvy0zils3jvp3.jpg",
+    measurement: "",
+  },
+  beefShort: {
+    name: "Beef Short Ribs",
+    thumbnail:
+      "https://preferredmeatsinc.com/wp-content/uploads/2017/06/short-ribs.jpg",
+    measurement: "",
+  },
+  groundBeef: { 
+    name: "Ground Beef", 
+    thumbnail: "https://img.freepik.com/premium-photo/raw-mince-beef-ground-meat-isolated-white-background_89816-45192.jpg",
+    measurement: "", 
+  },
+  porkBelly: { 
+    name: "Pork Belly", 
+    thumbnail: "https://www.shutterstock.com/image-photo/streaky-pork-isolated-on-white-260nw-2273600345.jpg",
+    measurement: "", 
+  },
+  groundPork: { 
+    name: "Ground Pork", 
+    thumbnail: "https://img.freepik.com/premium-photo/raw-meat-fresh-minced-pork-plate-isolated-white-background_106630-2393.jpg",
+    measurement: "", 
+  },
+  porkChop: { 
+    name: "Pork Chop", 
+    thumbnail: "https://t4.ftcdn.net/jpg/00/14/52/57/360_F_14525796_xgiHs1TmwKeB6SDP2wBBOmkknYR8Py5D.jpg",
+    measurement: "", 
+  },
+};
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+const ATLAS_URI = process.env.ATLAS_URI;
+
+await mongoose.connect(ATLAS_URI);
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Its running!');
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port: 3000");
+})
