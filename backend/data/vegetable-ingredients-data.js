ad663347c7f68acf3885db47962e6275c3f6c3ea
mikfairo <mikfairo123@gmail.com>
2025-11-19 09:07:25 -0500

    Added vegetable data set file

---
+++ backend/data/vegetable-ingredients-data.js
@@ -0,0 +1,30 @@
+const vegetableIngredients = {
+  type: "Vegetable",
+  greenOnion: {
+    name: "Green Onion",
+    thumbnail:
+      "https://t4.ftcdn.net/jpg/05/76/25/71/360_F_576257107_1b5v5hITQ2vKqgcSEZyjKay0uznCduNV.jpg",
+    measurement: "",
+  },
+  yellowOnion: {
+    name: "Yellow Onion",
+    thumbnail:
+      "https://static.vecteezy.com/system/resources/thumbnails/068/641/872/small/whole-and-halved-yellow-onion-with-green-sprouts-on-white-background-photo.jpeg",
+    measurement: "",
+  },
+  broccoli: {
+    name: "Broccoli",
+    thumbnail:
+      "https://as2.ftcdn.net/jpg/01/38/59/65/1000_F_138596527_vo9I8cKl20phDuIauIEfeQ2mWGw60Wjm.jpg",
+    measurement: "",
+  },
+   carrot: {
+    name: "Carrot",
+    thumbnail:
+      "https://as2.ftcdn.net/jpg/01/38/59/65/1000_F_138596527_vo9I8cKl20phDuIauIEfeQ2mWGw60Wjm.jpg",
+    measurement: "",
+  },
+  },
+};
+
+export default vegetableIngredients;
