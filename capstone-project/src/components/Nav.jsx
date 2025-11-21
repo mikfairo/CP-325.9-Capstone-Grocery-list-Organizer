f4e6ff745056beb91d64e1e17b2696e964ece8d4
mikfairo <mikfairo123@gmail.com>
2025-11-21 09:34:59 -0500

    Added browser route functionality and nav bar

---
+++ capstone-project/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Nav() {
+    const navigate = useNavigate()
+    return (
+        <div className=" bg-white flex flex-row justify-evenly p-2">
+            <h1 className="w-fit"
+            onClick={() => navigate('page1')}
+            >My Meals</h1>
+            <h1 className="w-fit"
+            onClick={() => navigate('page2')}
+            >Grocery List</h1>
+            <h1 className="w-fit"
+            onClick={() => navigate('page3')}
+            >Ingredients</h1>
+        </div>
+    )
+}
--- capstone-project/src/main.jsx
+++ capstone-project/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import HomePage from "./pages/Homepage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TestPageOne from "./pages/Testpage1.jsx";
+import TestPageTwo from "./pages/Testpage2.jsx";
+import TestPageThree from "./pages/Testpage3.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="page1" element={<TestPageOne />} />
+      <Route path="page2" element={<TestPageTwo />} />
+      <Route path="page3" element={<TestPageThree />} />
+    </Routes>
+  </BrowserRouter>
+);
