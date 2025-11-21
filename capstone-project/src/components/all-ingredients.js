c384a6e5420b7f510e5eb94d938ad0c7f7ff02b2
mikfairo <mikfairo123@gmail.com>
2025-11-20 21:18:34 -0500

    created frontend dataset to pull from backend to connect to App

---
+++ capstone-project/src/components/all-ingredients.js
@@ -0,0 +1,21 @@
+import vegetableIngredients from "../../../backend/data/vegetable-ingredients-data";
+import seafoodIngredients from "../../../backend/data/seafood-ingredients-data";
+import meatIngredients from "../../../backend/data/meat-ingredients-data";
+import fruitIngredients from "../../../backend/data/fruit-ingredients-data";
+import dryCannedIngredients from "../../../backend/data/dry-canned-ingredients-data";
+import dairyIngredients from "../../../backend/data/dairy-ingredients-data";
+import condimentsIngredients from "../../../backend/data/condiments-data";
+import bakingAndSpicesIngredients from "../../../backend/data/bakingspices-ingredients-data";
+
+const allCategories = [
+  vegetableIngredients,
+  seafoodIngredients,
+  meatIngredients,
+  fruitIngredients,
+  dryCannedIngredients,
+  dairyIngredients,
+  condimentsIngredients,
+  bakingAndSpicesIngredients,
+];
+
+export default allCategories;
