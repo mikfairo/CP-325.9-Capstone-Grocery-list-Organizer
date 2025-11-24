bef4534caa8ff826d5ab8d84b5f4451892e904ba
mikfairo <mikfairo123@gmail.com>
2025-11-24 01:20:11 -0500

    Fixed some styling and renamed files for readability

--- capstone-project/src/components/Nav.jsx
+++ capstone-project/src/components/Nav.jsx
@@ -4,16 +4,16 @@ export default function Nav() {
     const navigate = useNavigate()
     return (
         <div className=" bg-white flex flex-row justify-evenly p-2">
-            <h1 className="w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
+            <h1 className="text-lg w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
             onClick={() => navigate('/')}
             >Homepage</h1>
-            <h1 className="w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
+            <h1 className="text-lg w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
             onClick={() => navigate('/page1')}
-            >My Meals</h1>
-            <h1 className="w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
+            >My Recipes</h1>
+            <h1 className="text-lg w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
             onClick={() => navigate('/page2')}
             >Grocery List</h1>
-            <h1 className="w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
+            <h1 className="text-lg w-fit px-3 py-2 rounded hover:bg-gray-300 transition"
             onClick={() => navigate('/page3')}
             >Ingredients</h1>
         </div>
--- capstone-project/src/main.jsx
+++ capstone-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/Homepage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MyMealsPage from "./pages/My-meals-page.jsx";
+import MyRecipesPage from "./pages/My-recipes-page.jsx";
 import GroceryListPage from "./pages/Grocery-list-page.jsx";
 import IngredientsPage from "./pages/Ingredients-page.jsx";
 
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="page1" element={<MyMealsPage />} />
+      <Route path="page1" element={<MyRecipesPage />} />
       <Route path="page2" element={<GroceryListPage />} />
       <Route path="page3" element={<IngredientsPage />} />
     </Routes>
